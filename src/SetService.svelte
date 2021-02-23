dee9894fad03f544e9a316b5f6fd02c4160e5ca6
azamat <axrorxojayevazamat@gmail.com>
2021-02-23 18:08:28 +0500

    service

--- src/SetService.svelte
+++ src/SetService.svelte
@@ -2,77 +2,153 @@
     <div class="img">
         <img src={bgImage} alt="">
     </div>
-    <div>
-        {#each item as item }
-            <a class="card" href={('./:name', {item}, {item})}>
-                <h3>{item.name}</h3>
-            </a>
-        {/each}
+    <div class="container">
+        <div class="wrap">
+            <div class="col-left">
+                <div class="wraps">
+                    <div class="list">
+                        {#each items as item }
+                            <div class="item">
+                                <div class="item__wraps">
+                                    <a href="">
+                                        <span class="far fa-{item.icon}"></span>
+                                        <span class="title">{item.name}</span>
+                                    </a>
+                                </div>
+                            </div>
+                        {/each}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 
 <script>
     let bgImage = 'https://uztelecom.uz/images/1380x210_UZ.png';
 
-    let item =
+    let items =
         [
-            {name: 'GSM tariflar'},
-            {name: 'CDMA tariflar'},
-            {name: 'Internet tariflar'},
-            {name: 'GSM qoplangan hudud'},
-            {name: 'Saytni TAS-IX tarmog`i uchun tekshirish'},
-            {name: 'Tezlikni tekshirish'},
-            {name: 'Ofislarimiz'},
-            {name: 'Telefon apparatlari va qurilmalar'},
+            {
+                name: 'GSM tariflar',
+                icon: 'comment-dots'
+            },
+            {
+                name: 'CDMA tariflar',
+                icon: 'comment'
+            },
+            {
+                name: 'Internet tariflar',
+                icon: 'comment-alt'
+            },
+            {
+                name: 'GSM qoplangan hudud',
+                icon: 'comments'
+            },
+            {
+                name: 'Saytni TAS-IX tarmog`i uchun tekshirish',
+                icon: 'frown'
+            },
+            {
+                name: 'Tezlikni tekshirish',
+                icon: 'meh'
+            },
+            {
+                name: 'Ofislarimiz',
+                icon: 'smile'
+            },
+            {
+                name: 'Telefon apparatlari va qurilmalar',
+                icon: 'map'
+            },
         ];
 </script>
 
 <style type="text/scss">
-  section.set-service {
-    background-image: url("https://uztelecom.uz/images/section2-pattern-1.png");
-    background-size: cover;
-  }
+  .set-service {
+    padding: 60px 0 30px;
+    background-color: #f1f8ff;
+    position: relative;
+    overflow: hidden;
+    .container {
+      position: relative;
+      z-index: 1;
+      max-width: 1440px;
+      margin: 0 auto;
+      width: 100%;
+      .wrap {
+        font-size: 0;
+        line-height: 0;
+        margin: 0 -15px;
+        padding: 0 15px;
 
-  .img {
-    img {
-      width: 1300px;
-      height: 220px;
-      padding-left: 22px;
-      cursor: pointer;
-    }
-  }
+        .col-left {
+          display: inline-block;
+          vertical-align: top;
+          width: 66.66666667%;
 
-  :global(.card) {
-    background: white;
-    width: 150px;
-    height: 150px;
-    display: inline-block;
-    box-shadow: 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    margin: 24px;
-  }
+          .wraps {
+            padding: 0 15px;
 
-  div {
-    height: 100%;
-    display: grid;
-    grid-template-columns: repeat(4, 2fr);
-    width: 600px;
-    padding-left: 60px;
-    padding-right: 60px;
-    padding-top: 50px;
+            .list {
+              font-size: 0;
+              line-height: 0;
+              margin: 0 -15px;
 
-    a {
-      padding: 42px 34px;
-      text-align: center;
-      margin-bottom: 0;
-      border-radius: 5px;
+              .item {
+                list-style: none;
+                width: 25%;
+                display: inline-block;
+                vertical-align: top;
+                margin-bottom: 30px;
 
-      h3 {
-        color: #212f3d;
-        font-size: 20px;
-        font: 500 16px/16px Montserrat, sans-serif;
-        margin-bottom: 0;
-        margin-top: 150px;
+                .item__wraps {
+                  padding: 0 15px;
+                  text-align: center;
+
+                  a {
+                    display: block;
+                    font: 500 16px/16px Montserrat, sans-serif;
+                    color: #212f3d;
+                    text-decoration: none;
+                    border-radius: 6px;
+                    background-color: #fff;
+                    box-shadow: 0 3px 5px 0 rgba(33, 47, 61, .06);
+                    padding: 30px 5px 20px;
+                    transition: all .35s ease 0s;
+
+                    span {
+                      font-size: 72px;
+                      color: #8d959d;
+                      display: block;
+                      margin-bottom: 34px;
+                      transition: color .25s ease 0s;
+
+                      &.title {
+                        display: inline-block;
+                        min-height: 32px;
+                        font: 500 16px/16px Montserrat, sans-serif;
+                        color: #212f3d;
+                        margin-bottom: 0;
+                      }
+                    }
+
+                    &:hover {
+                      box-shadow: 0 20px 40px 0 rgba(34, 139, 214, .15);
+
+                      span {
+                        color: #228bd6;
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
       }
+
     }
+
   }
 </style>
